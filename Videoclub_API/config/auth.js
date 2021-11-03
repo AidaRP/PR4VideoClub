74d06b23f10d276b0cf7c7ec462fe8866d72d38d
AidaRP <aminingcrypto@gmail.com>
2021-11-03 06:20:37 +0100

    Actualización de comentarios en config auth.js

--- Videoclub_API/config/auth.js
+++ Videoclub_API/config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: process.env.AUTH_SECRET || "$1b4yg7832t7h28R%DRgt", //KEY USADA PARA ENCRIPTAR
-    expires: process.env.AUTH_EXPIRES || "24h", //DURACIÓN DEL TOKEN
-    rounds: process.env.AUTH_ROUNDS || 10 //VECES QUE SE ENCRIPTA LA CONTRASEÑA
+    secret: process.env.AUTH_SECRET || "$1b4yg7832t7h28R%DRgt", //Key usada para la encriptación.
+    expires: process.env.AUTH_EXPIRES || "24h", //Permanencia del Token.
+    rounds: process.env.AUTH_ROUNDS || 13 //Número de veces que la contraseña se encripta.
 }
